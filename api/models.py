abb43662e27c53b9d1b21c6fa29f751c1f9a69b6
M <m@mathsistor.com>
2018-08-06 13:46:56 -0500

    Change type of rating field of Review model

--- api/models.py
+++ api/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import Group, User
-from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from star_ratings.models import Rating
 
 
 class Review(models.Model):
     """A review made by a user."""
-    rating = GenericRelation(Rating)
+    RATING_CHOICES = (('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'))
+
+    rating = models.CharField(max_length=1, choices=RATING_CHOICES, default=RATING_CHOICES[0][0])
     title = models.CharField(max_length=64)
     summary = models.TextField(max_length=10_000)
     ip_address = models.GenericIPAddressField()
