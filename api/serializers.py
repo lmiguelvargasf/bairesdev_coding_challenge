730581e222b041b1224ed4c25014a0f558125e56
M <m@mathsistor.com>
2018-08-07 23:52:55 -0500

    Remove User serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,9 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 from .models import Review
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
-
-
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Review
-        fields = '__all__'
+        exclude = ('submission_date', 'ip_address', 'reviewer')
