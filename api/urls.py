d8166e68571328ed18a0c89f40730c5fe7a78ba6
M <m@mathsistor.com>
2018-08-07 22:50:27 -0500

    Add djangorestframework-jwt to dependencies and set it up

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from api import views
 
@@ -10,4 +11,7 @@ router.register('reviews', views.ReviewViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth-jwt/', obtain_jwt_token),
+    path('auth-jwt-refresh/', refresh_jwt_token),
+    path('auth-jwt-verify/', verify_jwt_token),
 ]
